82d4ae1334752c36e8a821c5e6f63e46b3a7c031
Jackie <1824561218@qq.com>
2023-10-17 17:52:43 +0800

    table

---
+++ src/components/TableList.vue
@@ -0,0 +1,67 @@
+<!--
+ * @Author: Jackie
+ * @Date: 2023-10-17 17:37:47
+ * @LastEditTime: 2023-10-17 17:49:07
+ * @LastEditors: Jackie
+ * @Description: 表格
+ * @FilePath: /my-anniversary-vue/src/components/TableList.vue
+ * @version: 
+-->
+<template>
+  <div class="table-box">
+    <div class="table">
+      <div class="title">
+        <h1>事件记录📝</h1>
+      </div>
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column prop="date" label="Date" width="180" />
+        <el-table-column prop="name" label="Name" width="180" />
+        <el-table-column prop="address" label="Address" />
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles'
+  }
+];
+</script>
+
+<style lang="scss" scoped>
+.table-box {
+  display: flex;
+  justify-content: center;
+  /* flex-direction: column; */
+  padding: 20px 0;
+  .table {
+    width: 1200px;
+    min-height: 600px;
+    .title {
+      color: #000;
+      font-family: PingFang SC;
+      font-size: 24px;
+      font-weight: 600;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: Jackie
  * @Date: 2023-06-25 09:58:10
- * @LastEditTime: 2023-10-17 16:59:06
+ * @LastEditTime: 2023-10-17 17:38:53
  * @LastEditors: Jackie
  * @Description: home
  * @FilePath: /my-anniversary-vue/src/views/Home.vue
@@ -9,114 +9,18 @@
 -->
 <template>
   <div class="home">
-    <div class="headtop">
-      <p class="time">{{ T }}</p>
-      <p class="date">{{ D }} {{ W }}</p>
-      <div class="topbg"></div>
-    </div>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
-    <p>123</p>
+    <HeadTop />
+    <TableList />
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted, computed } from 'vue';
-import dayjs from 'dayjs';
-
-let Times = '';
-const D = ref('');
-const T = ref('');
-const W = ref('');
-const week = ['日', '一', '二', '三', '四', '五', '六'];
-const getDateTime = () => {
-  const day = dayjs().format('YYYY-DD-MM HH:mm:ss').split(' ');
-  const day2 = dayjs().day();
-  D.value = day[0];
-  T.value = day[1];
-  W.value = '星期' + week[day2];
-  console.log(day);
-};
-
-onMounted(() => {
-  if (Times) {
-    clearInterval(Times);
-  }
-  Times = setInterval(() => {
-    getDateTime();
-  }, 1000);
-});
-onUnmounted(() => {
-  clearInterval(Times);
-});
+import HeadTop from '@/components/HeadTop.vue';
+import TableList from '@/components/TableList.vue';
 </script>
 
 <style lang="scss" scoped>
 .home {
-  .headtop {
-    position: relative;
-    z-index: 10;
-    user-select: none;
-    --wallpaper-blur: 0.5px;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    font-family: Inter, Helvetica, Arial, sans-serif;
-    .time {
-      color: rgb(255, 255, 255);
-      line-height: 120px;
-      font-size: 120px;
-      padding-bottom: 16px;
-      text-shadow: 0 8px 12px rgba(0, 0, 0, 0.1);
-    }
-    .date {
-      padding-bottom: 16px;
-      vertical-align: baseline;
-      font-size: 20px;
-      color: #fff;
-      line-height: 1.5;
-    }
-    .topbg {
-      filter: blur(0.5px);
-      background: url('@/assets/images/th.jpeg');
-      background-repeat: no-repeat;
-      background-size: cover;
-      background-position: 50%;
-      width: 100%;
-      height: 100%;
-
-      position: absolute;
-      z-index: -1;
-      &::after {
-        content: '';
-        position: absolute;
-        top: 0;
-        left: 0;
-        display: block;
-        user-select: none;
-        pointer-events: none;
-        z-index: 3;
-        opacity: 0.2;
-        width: 100%;
-        height: 100%;
-        background: radial-gradient(transparent 50%, black),
-          linear-gradient(transparent, black);
-      }
-      /* display: block;
-      opacity: 1;
-      transition: background-image 0.3s, background-color 0.3s;
-      transition-timing-function: ease; */
-      /* bottom: calc(var(--wallpaper-blur) * -2);
-      left: calc(var(--wallpaper-blur) * -2);
-      right: calc(var(--wallpaper-blur) * -2);
-      top: calc(var(--wallpaper-blur) * -2); */
-    }
-  }
 }
 
 @keyframes run {
